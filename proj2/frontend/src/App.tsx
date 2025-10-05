8bfc251e3e5cd6eccdb466000077e8f14718484f
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-10-05 03:25:51 -0400

    init: proj2

---
+++ proj2/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    fetch('/api')
+      .then((response) => response.json())
+      .then((data) => setMessage(data['The server is running']));
+  }, []);
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div>
+        <h1 className="text-3xl font-bold underline">{message}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
