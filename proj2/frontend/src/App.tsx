e75f2a2852ecc09143adf28b3ced4d4617c14f36
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-10-18 00:39:58 -0400

    feat: add Signup page and SignupField component with validation

--- proj2/frontend/src/App.tsx
+++ proj2/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router';
 import Welcome from './pages/Welcome';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
